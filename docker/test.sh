a2730a958e6030e82b570a711e0a8d590e5d9aa1
root <root@ip-172-31-53-203.ec2.internal>
2021-02-04 14:10:47 +0000

    adding files

---
+++ docker/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+TARGET=/incoming/
+PROCESSED=/processed/
+
+inotifywait -m -e create -e moved_to --format "%f" $TARGET \
+        | while read FILENAME
+                do
+                        echo Detected $FILENAME, moving and zipping
+                        echo Detected $FILENAME, moving and zipping >> log.txt
+			sendmail testt@gmail.com < log.txt
+			mv "$TARGET/$FILENAME" "$PROCESSED/$FILENAME"
+                        gzip "$PROCESSED/$FILENAME"
+                done
